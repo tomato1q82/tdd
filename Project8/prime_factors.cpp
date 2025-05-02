cbc5da36d6d8642ad34f8e85b740ace892a2ac88
namwook <tomato_zam@naver.com>
2025-05-02 13:56:20 +0900

    Test Of1

--- Project8/prime_factors.cpp
+++ Project8/prime_factors.cpp
@@ -1,3 +1,9 @@
-class PrimeFactors {
+#include <vector>
+using namespace std;
 
+class PrimeFactors {
+public:
+	vector<int> of(int i) {
+		return {};
+	}
 };
--- Project8/prime_factors_test.cpp
+++ Project8/prime_factors_test.cpp
@@ -1,6 +1,9 @@
+#include <vector>
 #include "gmock/gmock.h"
 #include "prime_factors.cpp"
 
 TEST(PrimeFactors, PrimeTest){
-
+	PrimeFactors prime_factor;
+	std::vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
